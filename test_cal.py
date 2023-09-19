9d6970b97a4896fe2fd316e331578ba8ae65f9ed
pokurialekhya <pokuri.alekhya@nagarro.com>
2023-09-19 13:18:01 +0530

    first commit

---
+++ test_cal.py
@@ -0,0 +1,12 @@
+from cal import *
+def test_add():
+    assert add(2,3)-- 5
+def test_sub():
+    assert sub(2,3)-- -1
+def test_mul():
+    assert mul(2,3)-- 6
+def test_div():
+    assert div(10,5)-- 2
+    
+    
+    
